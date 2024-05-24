610d2187a7cfd00ca2554b8198f356136e9fdf8e
DiegoProcopio0 <diegprocopio98@gmail.com>
2024-05-24 18:53:25 -0300

    feat: wip

---
+++ src/server/database/migrations/000_create_cidade.ts
@@ -0,0 +1,21 @@
+import { Knex } from "knex";
+import { ETablesNames } from "../ETablesNames";
+
+export async function up(kenx: Knex) {
+  return kenx.schema
+    .createTable(ETablesNames.cidade, (table) => {
+      table.bigIncrements("id").primary();
+      table.string("nome", 150).index().notNullable();
+
+      table.comment("Tabela usada para armazenar cidades no sistema.");
+    })
+    .then(() => {
+      console.log(`Tabela criada com sucesso! ${ETablesNames.cidade}`);
+    });
+}
+
+export async function down(kenx: Knex) {
+  return kenx.schema.dropTable(ETablesNames.cidade).then(() => {
+    console.log(`Tabela removida com sucesso! ${ETablesNames.cidade}`);
+  });
+}
