f5759bcc73452a9619bcecb7a10c92fa223bccdd
DiegoProcopio0 <diego.silva@gruponsc.com.br>
2024-06-02 12:39:15 -0300

    feat(services): create base `usuarios`

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,10 +1,9 @@
-import { ICidade } from "../../models";
-import { IPessoa } from "../../models/Pessoa";
+import { ICidade, IUsuario, IPessoa } from "../../models";
 
 declare module "knex/types/tables" {
   interface Tables {
     cidade: ICidade;
     pessoa: IPessoa;
-    // usuario: IUsuario;
+    usuario: IUsuario;
   }
 }
